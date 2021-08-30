673f0c8440cddd3558cad4fd3e874f5b6f628f36
FIRST_NAME LAST_NAME <analizaamparo4@gmail.com>
2021-08-30 17:20:05 +0800

    creating controller

---
+++ app/Http/Controllers/MyfirstController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyfirstController extends Controller
+{
+    public function index ()
+    {
+    	#code...
+    }
+}
